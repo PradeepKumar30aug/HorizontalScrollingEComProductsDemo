dfdd9238d6a15daf39204bfdebd17ce6923c43bd
Pradeep Kumar <pradeep30aug@gmail.com>
2019-09-13 23:53:00 +0530

    Initial commit

---
+++ app/src/main/java/com/nccevent/horizontalscrollingecomproductsdemo/Constants.java
@@ -0,0 +1,1 @@
+package com.nccevent.horizontalscrollingecomproductsdemo;public class Constants {    public final static String SERVER_IP="http://192.168.2.125";    public final static String SERVER_FOLDER="/fsprideapi/";    public final static String SERVER_FILE = "getBestProducts.php";}
